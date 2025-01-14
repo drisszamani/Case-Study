9b0e5d6a80acddde5079eb5513d2529cae598c3f
mac <drisszamani2@gmail.com>
2025-01-14 11:48:56 +0100

    Update pom.xml

--- car/src/test/java/config/TestConfig.java
+++ car/src/test/java/config/TestConfig.java
@@ -1,15 +1,16 @@
 package config;
 
+import com.example.car.clients.ClientRestClient;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import com.example.car.clients.ClientRestClient;
 
 @TestConfiguration
 public class TestConfig {
+
     @MockBean
     private ClientRestClient clientRestClient;
 
